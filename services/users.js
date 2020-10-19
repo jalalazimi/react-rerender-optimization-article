6b97e214ee1836aab40e0257d7df877b010c15aa
Jalal azimi <jalalazimi@gmail.com>
2020-10-19 09:29:02 +0100

    feat: add like button

--- services/users.js
+++ services/users.js
@@ -1,10 +1,10 @@
-async function get(){
-
-  const res = await fetch("https://api.github.com/users");
-
-  return res.json();
-
+async function get () {
+  const res = await fetch('https://api.github.com/users', {
+    headers: {
+      Authorization: process.env.GITHUB_TOKEN,
+    },
+  })
+  return res.json()
 }
-
 
 export default get;
